c0a92b67664d72b8c183c4a103490075faa2239b
OBRIEN66 <nicholas.obrien@ucc.on.ca>
2017-04-25 15:12:03 -0400

    Bulma

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Twitter App">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <title>Cooxle</title>
+
+    <link rel="stylesheet" type="text/css" href="bulma/css/bulma.css">
+</head>
+<body>
+	<?php include_once("nav.php") ?>
+
+	<script type="text/javascript" src="js/material.min.js"></script>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,12 @@
+<nav class="nav has-shadow">
+    <div class="container">
+        <div class="nav-left">
+            <a class="nav-item is-tab is-active">Home</a>
+            <a class="nav-item is-tab">Log In</a>
+            <a class="nav-item is-tab">Profile</a>
+        </div>
+        <div class="nav-right">
+            <a class="nav-item is-tab">Log Out</a>
+        </div>
+  </div>
+</nav>
