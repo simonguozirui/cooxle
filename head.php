ffb928fb68e7a1f51f3b40bee3ccbefdb1b183b3
OBRIEN66 <nicholas.obrien@ucc.on.ca>
2017-04-27 09:12:33 -0400

    create nav.php and head.php, rename edit.php => index.php

---
+++ head.php
@@ -0,0 +1,8 @@
+<head>
+	<meta charset="utf-8">
+	<title>Cooxle</title>
+	<link rel="stylesheet" type="text/css" href="bulma/css/bulma.css">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="Cooxle is a social media created by Nicholas O'Brien and Simon Guo for their grade 10 computer science class (ICS4U)">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
--- nav.php
+++ nav.php
@@ -1,20 +1,14 @@
 <nav class="nav has-shadow">
     <div class="container">
         <div class="nav-left">
-            <a class="nav-item">
+            <a class="nav-item" href="index.php">
              <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma logo">
             </a>
-            <a class="nav-item is-tab is-active">Home</a>
-            <a class="nav-item is-tab">Notifications</a>
+            <a class="nav-item is-tab is-active" href="index.php">Home</a>
+            <a class="nav-item is-tab" href="login.php">Notifications</a>
         </div>
         <div class="nav-right">
-            <a class="nav-item is-tab">
-            <figure class="image is-16x16" style="margin-right: 8px;">
-                <img src="http://bulma.io/images/jgthms.png">
-            </figure>
-            Profile
-            </a>
-            <a class="nav-item is-tab">Log Out</a>
+            <a class="nav-item is-tab" href="logout.php">Log Out</a>
         </div>
   </div>
 </nav>
