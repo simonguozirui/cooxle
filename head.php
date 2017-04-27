b273b3410173f003bcde02e6d2be739898c7c0ac
OBRIEN66 <nicholas.obrien@ucc.on.ca>
2017-04-27 09:54:17 -0400

    yoooo

--- head.php
+++ head.php
@@ -2,6 +2,7 @@
 	<meta charset="utf-8">
 	<title>Cooxle</title>
 	<link rel="stylesheet" type="text/css" href="bulma/css/bulma.css">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="description" content="Cooxle is a social media created by Nicholas O'Brien and Simon Guo for their grade 10 computer science class (ICS4U)">
     <meta name="viewport" content="width=device-width, initial-scale=1">
