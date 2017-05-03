a22edb47eeb91bf23f4393e5cd681353369443d5
simonguozirui <simonguozirui@gmail.com>
2017-05-03 12:09:11 -0400

    add about page menu

--- about.php
+++ about.php
@@ -1,8 +1,43 @@
 <html>
 	<?php include_once("required/head.php") ?>
 	<body>
-	<!-- <?php include_once("required/nav.php") ?> -->
+	<?php include_once("required/nav.php") ?>
+	<div class="container">
+		<div class="columns">
+			<div class="column is-one-quarter">
+				<aside class="menu">
+				  <p class="menu-label">
+				    General
+				  </p>
+				  <ul class="menu-list">
+				    <li><a>About Cooxle</a></li>
+				    <li><a>Instructions</a></li>
+						<li><a>Team</a></li>
+				  </ul>
+					<p class="menu-label">
+						Privacy
+				 </p>
+				 <ul class="menu-list">
+					 <li><a>Cookies</a></li>
+					 <li><a>User Information</a></li>
+				 </ul>
+				  <p class="menu-label">
+				    Developer Options
+				  </p>
+				  <ul class="menu-list">
+				    <li><a>Open-Source</a></li>
+						<li><a>Contribute</a></li>
+				  </ul>
+				</aside>
+			</div>
+			<div class="column">
+			</div>
+		</div>
+	</div>
 
-	<?php include_once('required/footer.php'); ?>
+
+
+
+
 	</body>
 </html>
