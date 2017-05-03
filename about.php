6ab103ccfa34add740e12dee52634b878acb9d65
OBRIEN66 <nicholas.obrien@ucc.on.ca>
2017-05-03 15:16:36 -0400

    Half of about

--- about.php
+++ about.php
@@ -35,17 +35,28 @@
 						<h1>General</h1>
 						<section>
 							<h2 id="about">About</h2>
-	  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+	  					<p>
+	  						Cooxle is a computer science project created by <a href="http://obrien.tech">Nicholas O'Brien</a> and <a href="http://simonguo.tech">Simon Guo</a> for their ICS4U computer science class. It is designed to work like twitter with easy-to-use styling and account systems. It is hosted on <a href="https://github.com/simonguozirui/cooxle">github</a> and under the MIT License.
+	  					</p>
 						</section>
 						<br>
 						<section>
 							<h2 id="features">Features</h2>
-	  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+	  					<p>
+	  						As said previously, Cooxle is supposed to work like twitter. It has account and posting systems in place to be used as a social media. To post, simply go to the home page and fill in the textboxes. To add a tag, enter your desired tag in the first textbox. Tags are not necessary, but they allow for users to find your post more easily. There is also a search feature. Users can search for posts by other users or for tags.
+	  					</p>
 						</section>
 						<br>
 						<section>
 							<h2 id="team">Team</h2>
-	  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+	  					<p>
+	  						The Cooxle team is composed of Nicholas O'Brien and Simon Guo. <br><br>
+	  						Nick: <br>
+	  						I am a UCC student, sailor, rower, and hobby developer. I spend a lot of time on the water and I love making boats go fast. I've been programming for 2 years (as of 2017) and I've had a lot of fun learning different programming languages. <br><br>
+
+	  						Simon:<br>
+	  						Sample text
+	  					</p>
 						</section>
 
 						<h1>Legal & Privacy</h1>
