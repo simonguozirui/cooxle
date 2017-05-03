0db8323d8181f9772a66c86c2dc1907a3f02c568
OBRIEN66 <nicholas.obrien@ucc.on.ca>
2017-05-03 14:53:24 -0400

    links on about

--- about.php
+++ about.php
@@ -2,82 +2,77 @@
 	<?php include_once("required/head.php") ?>
 	<body>
 	<?php include_once("required/nav.php") ?>
-	<div class="container">
-		<div class="columns">
-			<div class="column is-2">
-				<aside class="menu">
-				  <p class="menu-label">
-				    General
-				  </p>
-				  <ul class="menu-list">
-				    <li><a href="#about">About Cooxle</a></li>
-				    <li><a>Features</a></li>
-						<li><a>Team</a></li>
-				  </ul>
-					<p class="menu-label">
-						Legal & Privacy
-				 </p>
-				 <ul class="menu-list">
-					 <li><a>Privacy</a></li>
-					 <li><a>User Information</a></li>
-				 </ul>
-				  <p class="menu-label">
-				    Developer Options
-				  </p>
-				  <ul class="menu-list">
-				    <li><a>Open-Source</a></li>
-						<li><a>Contribute</a></li>
-				  </ul>
-				</aside>
-			</div>
-			<div class="column">
-				<div class="content">
-					<h1>General</h1>
-					<section>
-						<h2>About</h2>
-  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
-					</section>
-					<br>
-					<section>
-						<h2>Features</h2>
-  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
-					</section>
-					<br>
-					<section>
-						<h2>Team</h2>
-  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
-					</section>
+		<div class="container">
+			<div class="columns">
+				<div class="column is-2">
+					<aside class="menu">
+					  <p class="menu-label">
+					    General
+					  </p>
+					  <ul class="menu-list">
+					    <li><a href="#about">About Cooxle</a></li>
+					    <li><a href="#features">Features</a></li>
+						<li><a href="#team">Team</a></li>
+					  </ul>
+						<p class="menu-label">
+							Legal & Privacy
+					 </p>
+					 <ul class="menu-list">
+						 <li><a href="#privacy">Privacy</a></li>
+						 <li><a href="#info">User Information</a></li>
+					 </ul>
+					  <p class="menu-label">
+					    Developer Options
+					  </p>
+					  <ul class="menu-list">
+					    <li><a href="#open-source">Open-Source</a></li>
+						<li><a href="#contribute">Contribute</a></li>
+					  </ul>
+					</aside>
+				</div>
+				<div class="column">
+					<div class="content">
+						<h1>General</h1>
+						<section>
+							<h2 id="about">About</h2>
+	  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+						</section>
+						<br>
+						<section>
+							<h2 id="features">Features</h2>
+	  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+						</section>
+						<br>
+						<section>
+							<h2 id="team">Team</h2>
+	  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+						</section>
 
-					<h1>Legal & Privacy</h1>
-					<section>
-						<h2>Privacy</h2>
-  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
-					</section>
-					<br>
-					<section>
-						<h2>User Information</h2>
-  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
-					</section>
+						<h1>Legal & Privacy</h1>
+						<section>
+							<h2 id="privacy">Privacy</h2>
+	  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+						</section>
+						<br>
+						<section>
+							<h2 id="info">User Information</h2>
+	  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+						</section>
 
-					<h1>Developer Option</h1>
-					<section>
-						<h2>Open-Source</h2>
-  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
-					</section>
-					<br>
-					<section>
-						<h2>Contribute</h2>
-  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
-					</section>
+						<h1>Developer Option</h1>
+						<section>
+							<h2 id="open-source">Open-Source</h2>
+	  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+						</section>
+						<br>
+						<section>
+							<h2 id="contribute">Contribute</h2>
+	  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+						</section>
 
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-
-
-
-
-
 	</body>
 </html>
