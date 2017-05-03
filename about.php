dd948d73b83f7848669782a52acf5e59b737b6c6
simonguozirui <simonguozirui@gmail.com>
2017-05-03 12:22:47 -0400

    layout of about page content

--- about.php
+++ about.php
@@ -4,21 +4,21 @@
 	<?php include_once("required/nav.php") ?>
 	<div class="container">
 		<div class="columns">
-			<div class="column is-one-quarter">
+			<div class="column is-2">
 				<aside class="menu">
 				  <p class="menu-label">
 				    General
 				  </p>
 				  <ul class="menu-list">
-				    <li><a>About Cooxle</a></li>
-				    <li><a>Instructions</a></li>
+				    <li><a href="#about">About Cooxle</a></li>
+				    <li><a>Features</a></li>
 						<li><a>Team</a></li>
 				  </ul>
 					<p class="menu-label">
-						Privacy
+						Legal & Privacy
 				 </p>
 				 <ul class="menu-list">
-					 <li><a>Cookies</a></li>
+					 <li><a>Privacy</a></li>
 					 <li><a>User Information</a></li>
 				 </ul>
 				  <p class="menu-label">
@@ -31,6 +31,46 @@
 				</aside>
 			</div>
 			<div class="column">
+				<div class="content">
+					<h1>General</h1>
+					<section>
+						<h2>About</h2>
+  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+					</section>
+					<br>
+					<section>
+						<h2>Features</h2>
+  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+					</section>
+					<br>
+					<section>
+						<h2>Team</h2>
+  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+					</section>
+
+					<h1>Legal & Privacy</h1>
+					<section>
+						<h2>Privacy</h2>
+  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+					</section>
+					<br>
+					<section>
+						<h2>User Information</h2>
+  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+					</section>
+
+					<h1>Developer Option</h1>
+					<section>
+						<h2>Open-Source</h2>
+  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+					</section>
+					<br>
+					<section>
+						<h2>Contribute</h2>
+  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+					</section>
+
+				</div>
 			</div>
 		</div>
 	</div>
